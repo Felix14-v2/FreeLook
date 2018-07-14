40379573d0b04deeb728321f0217222a947b69b4
jmilthedude <jmiltheman@gmail.com>
2018-07-13 20:14:24 -0500

    1.0 Release

--- src/main/java/net/thedudemc/freelook/FreeLook.java
+++ src/main/java/net/thedudemc/freelook/FreeLook.java
@@ -1,38 +1,39 @@
 package net.thedudemc.freelook;
 
+import org.lwjgl.input.Keyboard;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.thedudemc.freelook.client.ClientProxy;
 import net.thedudemc.freelook.util.Reference;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, clientSideOnly = true)
 public class FreeLook {
+	
+	public static KeyBinding keyBinding;
 
 	@Instance
 	public static FreeLook instance;
-	
-	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS)
-	public static ClientProxy proxy;
-	
+
+
 	@EventHandler
-	public static void PreInit(FMLPreInitializationEvent event)
-	{
+	public static void PreInit(FMLPreInitializationEvent event) {
 	}
-	
+
 	@EventHandler
-	public static void init(FMLInitializationEvent event)
-	{
-	ClientProxy.init();
+	public static void init(FMLInitializationEvent event) {
+
+		keyBinding = new KeyBinding("key.freelook.desc", Keyboard.KEY_LMENU, "key.freelook.category");
+		ClientRegistry.registerKeyBinding(keyBinding);
 	}
-	
+
 	@EventHandler
-	public static void PostInit(FMLPostInitializationEvent event)
-	{
-		
+	public static void PostInit(FMLPostInitializationEvent event) {
+
 	}
 }
--- src/main/java/net/thedudemc/freelook/util/EventHandler.java
+++ src/main/java/net/thedudemc/freelook/util/EventHandler.java
@@ -5,28 +5,34 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.thedudemc.freelook.FreeLook;
 import net.thedudemc.freelook.client.Camera;
-import net.thedudemc.freelook.client.ClientProxy;
 
 @EventBusSubscriber
 public class EventHandler {
 
 	public static boolean altIsDown = false;
-	// private static long lastAltClick = 0;
-	// private static boolean altIsPressed = false;
+	public static boolean last = false;
 
 	@SubscribeEvent
 	public static void onRenderTick(TickEvent.RenderTickEvent event) {
-		Camera.update(event.phase == Phase.START);
+		if (FreeLook.keyBinding.isKeyDown() && last) {
+			Camera.update(event.phase == Phase.START);
+		}
+		if (!FreeLook.keyBinding.isKeyDown() && last) {
+			Camera.reset(); 
+			last = false;
+		}
 	}
 
 	@SubscribeEvent
 	public static void onKey(KeyInputEvent event) {
-		if (ClientProxy.keyBinding.isKeyDown()) {
+		if (FreeLook.keyBinding.isPressed()) {
 			if (!altIsDown) {
 				Camera.setCamera();
 			}
 			altIsDown = true;
+			last = true;
 		} else {
 			altIsDown = false;
 		}
